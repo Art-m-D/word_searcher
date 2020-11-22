a4bc062848b2ae08b2a12e8d8dbf5ce142a1900b
artem.dobrodon <artem.dobrodon@skylum.com>
2020-11-23 01:14:19 +0200

    performance optimization#2

--- src/all_direction_lib.py
+++ src/all_direction_lib.py
@@ -1,57 +1,36 @@
 def _get_word_right(letters_grid, h, w, height, width):
-    word = ''
-    for i in range(w, width):
-        word += letters_grid[h][i]
-    return word
+    return ''.join([letters_grid[h][i] for i in range(w, width)])
 
 
 def _get_word_left(letters_grid, h, w, height, width):
-    word = ''
-    for i in range(w, -1, -1):
-        word += letters_grid[h][i]
-    return word
+    return ''.join([letters_grid[h][i] for i in range(w, -1, -1)])
 
 
 def _get_word_up(letters_grid, h, w, height, width):
-    word = ''
-    for i in range(h, -1, -1):
-        word += letters_grid[i][w]
-    return word
+    return ''.join([letters_grid[i][w] for i in range(h, -1, -1)])
 
 
 def _get_word_down(letters_grid, h, w, height, width):
     word = ''
     for i in range(h, height):
         word += letters_grid[i][w]
-    return word
+    return ''.join([letters_grid[i][w] for i in range(h, height)])
 
 
 def _get_word_right_down(letters_grid, h, w, height, width):
-    word = ''
-    for i, j in zip(range(h, height), range(w, width)):
-        word += letters_grid[i][j]
-    return word
+    return ''.join([letters_grid[i][j] for i, j in zip(range(h, height), range(w, width))])
 
 
 def _get_word_right_up(letters_grid, h, w, height, width):
-    word = ''
-    for i, j in zip(range(h, -1, -1), range(w, width)):
-        word += letters_grid[i][j]
-    return word
+    return ''.join([letters_grid[i][j] for i, j in zip(range(h, -1, -1), range(w, width))])
 
 
 def _get_word_left_down(letters_grid, h, w, height, width):
-    word = ''
-    for i, j in zip(range(h, height), range(w, -1, -1)):
-        word += letters_grid[i][j]
-    return word
+    return ''.join([letters_grid[i][j] for i, j in zip(range(h, height), range(w, -1, -1))])
 
 
 def _get_word_left_up(letters_grid, h, w, height, width):
-    word = ''
-    for i, j in zip(range(h, -1, -1), range(w, -1, -1)):
-        word += letters_grid[i][j]
-    return word
+    return ''.join([letters_grid[i][j] for i, j in zip(range(h, -1, -1), range(w, -1, -1))])
 
 
 all_direction_words = [
